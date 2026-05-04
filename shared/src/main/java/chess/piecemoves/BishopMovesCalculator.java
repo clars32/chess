389e2d17b5b9c9488fe7382fc902bc0e0a44de30
Carter Larsen <clars32@byu.edu>
2026-05-04 00:11:27 -0600

    All BishopMoveTests pass

--- shared/src/main/java/chess/piecemoves/BishopMovesCalculator.java
+++ shared/src/main/java/chess/piecemoves/BishopMovesCalculator.java
@@ -6,50 +6,113 @@ import java.util.Collection;
 import chess.ChessBoard;
 import chess.ChessMove;
 import chess.ChessPosition;
+import chess.ChessPiece;
 
 public class BishopMovesCalculator implements PieceMovesCalculator {
     
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition position) {
 
-        ArrayList<ChessMove> valid_moves = new ArrayList<ChessMove>();
+        ArrayList<ChessMove> validMoves = new ArrayList<ChessMove>();
+
+        ChessPiece currentPiece = board.getPiece(position);
 
         // Check moves up and to the right
-        int new_row = position.getRow();
-        int new_col = position.getColumn();
-        while (new_row <= 7 && new_col <= 7) {
-            new_row += 1;
-            new_col += 1;
-            valid_moves.add(new ChessMove(position, new ChessPosition(new_row, new_col), null));
+        int newRow = position.getRow() + 1;
+        int newCol = position.getColumn() + 1;
+
+        while (newRow <= 8 && newCol <= 8) {
+
+            ChessPosition nextPosition = new ChessPosition(newRow, newCol);
+            ChessPiece pieceAtNext = board.getPiece(nextPosition);
+
+            if (pieceAtNext == null) {
+                validMoves.add(new ChessMove(position, nextPosition, null));
+            } else if (pieceAtNext.getTeamColor() != currentPiece.getTeamColor()) { // Capture
+                validMoves.add(new ChessMove(position, nextPosition, null));
+                break;
+            }
+            else {
+                break;
+            }
+
+            newRow++;
+            newCol++;
+
         }
 
         // Check moves up and to the left
-        new_row = position.getRow();
-        new_col = position.getColumn();
-        while (new_row <= 7 && new_col >= 2) {
-            new_row += 1;
-            new_col -= 1;
-            valid_moves.add(new ChessMove(position, new ChessPosition(new_row, new_col), null));
+        newRow = position.getRow() + 1;
+        newCol = position.getColumn() - 1;
+
+        while (newRow <= 8 && newCol >= 1) {
+
+            ChessPosition nextPosition = new ChessPosition(newRow, newCol);
+            ChessPiece pieceAtNext = board.getPiece(nextPosition);
+            
+            if (pieceAtNext == null) {
+                validMoves.add(new ChessMove(position, nextPosition, null));
+            } else if (pieceAtNext.getTeamColor() != currentPiece.getTeamColor()) { // Capture
+                validMoves.add(new ChessMove(position, nextPosition, null));
+                break;
+            }
+            else {
+                break;
+            }
+
+            newRow++;
+            newCol--;
+
         }
 
         // Check moves down and to the left
-        new_row = position.getRow();
-        new_col = position.getColumn();
-        while (new_row >= 2 && new_col >= 2) {
-            new_row -= 1;
-            new_col -= 1;
-            valid_moves.add(new ChessMove(position, new ChessPosition(new_row, new_col), null));
+        newRow = position.getRow() - 1;
+        newCol = position.getColumn() - 1;
+
+        while (newRow >= 1 && newCol >= 1) {
+
+            ChessPosition nextPosition = new ChessPosition(newRow, newCol);
+            ChessPiece pieceAtNext = board.getPiece(nextPosition);
+            
+            if (pieceAtNext == null) {
+                validMoves.add(new ChessMove(position, nextPosition, null));
+            } else if (pieceAtNext.getTeamColor() != currentPiece.getTeamColor()) { // Capture
+                validMoves.add(new ChessMove(position, nextPosition, null));
+                break;
+            }
+            else {
+                break;
+            }
+
+            newRow--;
+            newCol--;
+
         }
 
         // Check moves down and to the right
-        new_row = position.getRow();
-        new_col = position.getColumn();
-        while (new_row >= 2 && new_col <= 7) {
-            new_row -= 1;
-            new_col += 1;
-            valid_moves.add(new ChessMove(position, new ChessPosition(new_row, new_col), null));
-        }
+        newRow = position.getRow() - 1;
+        newCol = position.getColumn() + 1;
 
-        return valid_moves;
+        while (newRow >= 1 && newCol <= 8) {
+
+            ChessPosition nextPosition = new ChessPosition(newRow, newCol);
+            ChessPiece pieceAtNext = board.getPiece(nextPosition);
+            
+            if (pieceAtNext == null) {
+                validMoves.add(new ChessMove(position, nextPosition, null));
+            } else if (pieceAtNext.getTeamColor() != currentPiece.getTeamColor()) { // Capture
+                validMoves.add(new ChessMove(position, nextPosition, null));
+                break;
+            }
+            else {
+                break;
+            }
+
+            newRow--;
+            newCol++;
+
+        }
+        
+        return validMoves;
 
     }
 
