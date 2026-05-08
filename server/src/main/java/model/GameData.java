c5b29a743194bdec42410bebc7f9022d7ff044bc
Carter Larsen <clars32@byu.edu>
2026-05-07 22:52:40 -0600

    Created appropriate records classes

---
+++ server/src/main/java/model/GameData.java
@@ -0,0 +1,11 @@
+package model;
+
+import chess.ChessGame;
+
+public record GameData (
+    int gameID,
+    String whiteUserName,
+    String blackUserName,
+    String gameName,
+    ChessGame game
+) {}
