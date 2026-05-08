da1f9ef1087f8de4325dd17923965b86046ea934
Carter Larsen <clars32@byu.edu>
2026-05-07 23:15:53 -0600

    clear() test written

---
+++ server/src/test/java/service/ClearServiceTest.java
@@ -0,0 +1,22 @@
+package service;
+
+import dataaccess.*;
+import model.UserData;
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ClearServiceTest {
+    
+    @Test
+    void clearRemovesAllData() throws DataAccessException {
+
+        DataAccess dao = new MemoryDataAccess();
+        dao.createUser(new UserData("carter", "pass", "test@test.com"));
+
+        new ClearService(dao).clear();
+
+        assertNull(dao.getUser("carter"));
+
+    }
+    
+}
