fa7fe16c3ea180cc5f7b0d8ca19a3f5966c790c1
Carter Larsen <clars32@byu.edu>
2026-05-07 23:11:19 -0600

    clear() endpoint implemented

---
+++ server/src/main/java/dataaccess/MemoryDataAccess.java
@@ -0,0 +1,80 @@
+package dataaccess;
+
+import model.*;
+import java.util.*;
+
+public class MemoryDataAccess implements DataAccess {
+    
+
+    // Store necessary info
+
+    private final Map<String, UserData> users = new HashMap<>();
+    private final Map<String, AuthData> auths = new HashMap<>();
+    private final Map<String, GameData> games = new HashMap<>();
+    private int nextGameId = 1;
+
+    // clear()
+
+    @Override
+    public void clear() {
+        users.clear();
+        auths.clear();
+        games.clear();
+        nextGameId = 1;
+    }
+
+    @Override
+    public UserData getUser(String username) throws DataAccessException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getUser'");
+    }
+
+    @Override
+    public void createUser(UserData user) throws DataAccessException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createUser'");
+    }
+
+    @Override
+    public void createAuth(AuthData auth) throws DataAccessException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createAuth'");
+    }
+
+    @Override
+    public AuthData getAuth(String authToken) throws DataAccessException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAuth'");
+    }
+
+    @Override
+    public void deleteAuth(String authToken) throws DataAccessException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteAuth'");
+    }
+
+    @Override
+    public Collection<GameData> listGames() throws DataAccessException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'listGames'");
+    }
+
+    @Override
+    public int createGame(GameData game) throws DataAccessException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createGame'");
+    }
+
+    @Override
+    public GameData getGame(int gameID) throws DataAccessException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getGame'");
+    }
+
+    @Override
+    public void updateGame(GameData game) throws DataAccessException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updateGame'");
+    }
+
+}
--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,15 +1,31 @@
 package server;
 
+import dataaccess.DataAccess;
+import dataaccess.MemoryDataAccess;
+import service.*;
 import io.javalin.*;
+import com.google.gson.Gson;
+import java.util.Map;
 
 public class Server {
 
     private final Javalin javalin;
+    private final DataAccess dataAccess = new MemoryDataAccess();
+    private final ClearService clearService = new ClearService(dataAccess);
+    private final Gson gson = new Gson();
 
     public Server() {
         javalin = Javalin.create(config -> config.staticFiles.add("web"));
 
-        // Register your endpoints and exception handlers here.
+        javalin.delete("/db", ctx -> {
+            clearService.clear();
+            ctx.status(200).result("{}");
+        });
+
+        // Handle any exceptions
+        javalin.exception(Exception.class, (e, ctx) -> {
+            ctx.status(500).result(gson.toJson(Map.of("message", "Error: " + e.getMessage())));
+        });
 
     }
 
@@ -21,4 +37,5 @@ public class Server {
     public void stop() {
         javalin.stop();
     }
+    
 }
