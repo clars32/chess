e3edc8e0d9c19753503e59fadae11b9ea2f6aedc
Carter Larsen <clars32@byu.edu>
2026-05-07 23:02:04 -0600

    Create DataAccess interface

---
+++ server/src/main/java/dataaccess/DataAccess.java
@@ -0,0 +1,25 @@
+package dataaccess;
+
+import model.*;
+import java.util.Collection;
+
+public interface DataAccess {
+    
+    void clear() throws DataAccessException;
+
+    // User methods
+    UserData getUser(String username) throws DataAccessException;
+    void createUser(UserData user) throws DataAccessException;
+
+    // Auth methods
+    void createAuth(AuthData auth) throws DataAccessException;
+    AuthData getAuth(String authToken) throws DataAccessException;
+    void deleteAuth(String authToken) throws DataAccessException;
+
+    // Game methods
+    Collection<GameData> listGames() throws DataAccessException;
+    int createGame(GameData game) throws DataAccessException;
+    GameData getGame(int gameID) throws DataAccessException;
+    void updateGame(GameData game) throws DataAccessException;
+
+}
