6d041aa4dbfba32041a35b1d0896e7b4728c34f7
Carter Larsen <clars32@byu.edu>
2026-05-20 23:14:18 -0600

    SQL clear and creation code

--- server/src/main/java/dataaccess/SQLDataAccess.java
+++ server/src/main/java/dataaccess/SQLDataAccess.java
@@ -1,6 +1,7 @@
 package dataaccess;
 
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Collection;
 
 import com.google.gson.Gson;
@@ -56,9 +57,48 @@ public class SQLDataAccess implements DataAccess {
 
     @Override
     public void clear() throws DataAccessException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'clear'");
+        try (var conn = DatabaseManager.getConnection()) {
+            for (var table: new String[]{"auth", "games", "users"}) {
+                try (var ps = conn.prepareStatement("DELETE FROM " + table)) {
+                    ps.executeUpdate();
+                }
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException("clear failed", e);
+        }
     }
+
+    @Override
+    public void createAuth(AuthData auth) throws DataAccessException {
+        var sql = "INSERT INTO auth (authToken, username) VALUES (?, ?)";
+        try (var conn = DatabaseManager.getConnection();
+            var ps = conn.prepareStatement(sql)) {
+                ps.setString(1, auth.authToken());
+                ps.setString(2, auth.username());
+                ps.executeUpdate();
+        } catch (SQLException e) {
+            throw new DataAccessException("createAuth failed", e);
+        }
+    }
+
+    @Override
+    public int createGame(GameData game) throws DataAccessException {
+        var sql = "INSERT INTO games (whiteUsername, blackUsername, gameName, game) VALUES (?, ?, ?, ?)";
+        try (var conn = DatabaseManager.getConnection();
+            var ps = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+                ps.setString(1, game.whiteUsername());
+                ps.setString(2, game.blackUsername());
+                ps.setString(3, game.gameName());
+                ps.setString(4, gson.toJson(game.game()));
+                ps.executeUpdate();
+                var keys = ps.getGeneratedKeys();
+                keys.next();
+                return keys.getInt(1);
+        } catch (SQLException e) {
+            throw new DataAccessException("createGame failed", e);
+        }
+    }
+
 
     @Override
     public UserData getUser(String username) throws DataAccessException {
@@ -70,12 +110,6 @@ public class SQLDataAccess implements DataAccess {
     public void createUser(UserData user) throws DataAccessException {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'createUser'");
-    }
-
-    @Override
-    public void createAuth(AuthData auth) throws DataAccessException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'createAuth'");
     }
 
     @Override
@@ -94,12 +128,6 @@ public class SQLDataAccess implements DataAccess {
     public Collection<GameData> listGames() throws DataAccessException {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'listGames'");
-    }
-
-    @Override
-    public int createGame(GameData game) throws DataAccessException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'createGame'");
     }
 
     @Override
