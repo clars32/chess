df49648b2ba8b4aeaad3a89d4469901e89ce2124
Carter Larsen <clars32@byu.edu>
2026-06-01 11:13:29 -0600

    register() tests written

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,18 +1,26 @@
 package client;
 
+import model.AuthData;
+import model.GameData;
+
 import org.junit.jupiter.api.*;
 import server.Server;
 
+import java.util.Collection;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ServerFacadeTests {
 
     private static Server server;
+    private static ServerFacade facade;
 
     @BeforeAll
     public static void init() {
         server = new Server();
-        var port = server.run(0);
+        int port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
+        facade = new ServerFacade(port);
     }
 
     @AfterAll
@@ -20,10 +28,33 @@ public class ServerFacadeTests {
         server.stop();
     }
 
+    @BeforeEach
+    void clearDatabase() throws ResponseException {
+        facade.clear();
+    }
+
+    // register()
 
     @Test
-    public void sampleTest() {
-        Assertions.assertTrue(true);
+    void registerSuccess() throws Exception {
+
+        AuthData auth = facade.register("player1", "password", "p1@email.com");
+
+        assertNotNull(auth);
+        assertEquals("player1", auth.username());
+        assertNotNull(auth.authToken());
+        assertTrue(auth.authToken().length() > 10);
+
+    }
+
+    @Test
+    void registerDuplicateUsernameFails() throws Exception {
+
+        facade.register("player1", "password", "p1@email.com");
+
+        ResponseException ex = assertThrows(ResponseException.class, () ->
+            facade.register("player1", "different", "other@email.com"));
+        assertEquals(403, ex.statusCode());
     }
 
 }
